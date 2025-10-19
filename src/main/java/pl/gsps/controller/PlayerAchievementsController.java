31aadfe582281de7cd019d4225eec251463541da
Pkow <pkow1999@gmail.com>
2025-10-19 16:58:13 +0200

    Added Controller for player's achievement and set it up with default queries

---
+++ src/main/java/pl/gsps/controller/PlayerAchievementsController.java
@@ -0,0 +1,61 @@
+package pl.gsps.controller;
+
+import okhttp3.*;
+import pl.gsps.models.PlayerAchievements;
+import pl.gsps.service.AchievementsService;
+import pl.gsps.utility.ConfigurationReader;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+public class PlayerAchievementsController {
+    private final String BASE_URI = ConfigurationReader.readConfigurationFile("BASE_URI");
+    private final String API_KEY = ConfigurationReader.readConfigurationFile("API_KEY");
+    private final String GAME_ID = ConfigurationReader.readConfigurationFile("GAME_ID");
+    private final String PLAYER_ID = ConfigurationReader.readConfigurationFile("STEAM_ID");
+    private AchievementsService service;
+    public PlayerAchievementsController() {
+        setupService();
+    }
+
+    public PlayerAchievements getSteamAchievements() {
+        Call<PlayerAchievements> call = service.getPlayerAchievements();
+        try {
+            Response<PlayerAchievements> response = call.execute();
+            return response.body();
+        } catch (Exception ex) {
+            System.out.println("Couldn't get achievements due to exception: " + ex);
+        }
+        return null;
+    }
+    private void setupService() {
+        Interceptor clientInterceptor = setRepeatingQueryParameters();
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(clientInterceptor)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URI)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(client)
+                .build();
+
+        service = retrofit.create(AchievementsService.class);
+    }
+
+    private Interceptor setRepeatingQueryParameters() {
+        return chain -> {
+            Request request = chain.request();
+            HttpUrl url = request.url().newBuilder()
+                    .addQueryParameter("key", API_KEY)
+                    .addQueryParameter("appid", GAME_ID)
+                    .addQueryParameter("steamid", PLAYER_ID)
+                    .addQueryParameter("l", "en")
+                    .build();
+            request = request.newBuilder().url(url).build();
+            return chain.proceed(request);
+        };
+    }
+}
